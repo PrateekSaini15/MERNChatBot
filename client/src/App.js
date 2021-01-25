a655dc264598788b3d50f1c542bfe48ca1994f1e
Prateek Saini <prateeksaini15@yahoo.com>
2021-01-25 13:16:20 +0530

    Add action provider, message parser and config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import Chatbot from "react-chatbot-kit";
 
+import ActionProvider from "./BotChat/ActionProvider";
+import MessageParser from "./BotChat/MessageParser";
+import config from "./BotChat/config";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
       </header>
     </div>
   );
