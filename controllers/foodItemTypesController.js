bff2704acd57bc1f8c3e552ca236b0317f3eab2e
Prateek Saini <prateeksaini15@yahoo.com>
2021-02-08 17:37:07 +0530

    Select only active types

--- controllers/foodItemTypesController.js
+++ controllers/foodItemTypesController.js
@@ -3,7 +3,7 @@ import foodItemType from "../model/ItemTypes.js";
 export function getfoodItemTypesByRestaurant(req, res) {
   const restaurantId = req.body.RestaurantId;
   foodItemType
-    .find({ RestaurantId: restaurantId })
+    .find({ RestaurantId: restaurantId, IsActive: true })
     .then((types) => res.status(201).json({ types }))
     .catch((error) => res.status(400).json({ error }));
 }
